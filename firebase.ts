7ec8882c049d7361f129b3f2e193d23a7ac912b9
Matthew Nte <nte@grace.health>
2022-09-14 11:13:48 +0100

    convert fbservice account key from json to string

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,9 @@
 import admin from 'firebase-admin'
+import { firebaseServiceAccount } from './src/config'
 
-const serviceAccount = require('./fbServiceAccount.json')
+const serviceAccount = JSON.parse(
+  Buffer.from(firebaseServiceAccount!, 'base64').toString('ascii'),
+)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,5 @@
-import 'dotenv/config';
+import 'dotenv/config'
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 3000
+export const firebaseServiceAccount =
+  process.env.FIREBASE_SERVICE_ACCOUNT_BASE64
